6c1ab94141285f2ec24af69dd7d73f4b72fbb5e0
ViswaaE <viswaaeswaran@gmail.com>
2025-08-30 20:07:11 +0530

    landing page component fully added without correction made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import FaqComponent from "@/components/Faq";
+import StatsBanner from "@/components/Stats";
 import AboutUsSection from "@/pages/Landing_page/about-us-section";
+import BlogSection from "@/pages/Landing_page/Blogsection";
 import HeroSection from "@/pages/Landing_page/Herosection";
 import ServicesSection from "@/pages/Landing_page/services-section";
+import TestimonialSection from "@/pages/Landing_page/Testimonial";
 
 
 export default function Home() {
@@ -9,6 +13,10 @@ export default function Home() {
       <HeroSection />
       <AboutUsSection />
       <ServicesSection />
+      <FaqComponent />
+      <StatsBanner />
+      <TestimonialSection />
+      <BlogSection /> 
     </div>
   );
 }
