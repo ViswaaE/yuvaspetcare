466dde47bc172231eac241d15af789eeaac3e78f
ViswaaE <viswaaeswaran@gmail.com>
2025-09-17 07:11:09 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 enables static HTML export
+  basePath: "/yuvaspetcare", // 👈 replace with your repo name
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
 };
 
 export default nextConfig;
